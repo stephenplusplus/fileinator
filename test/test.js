2e207fef5968554e4c91f8a776c09bddee151201
Justin Beckwith <justin.beckwith@gmail.com>
2016-03-19 21:37:31 -0700

    add lib tests

---
+++ test/test.js
@@ -0,0 +1,55 @@
+const assert = require('assert');
+const fileinator = require('../lib/fileinator');
+const sizeParser = require('filesize-parser');
+const fs = require('fs');
+const uuid = require('node-uuid');
+
+describe('fileinator', function() {
+  
+  it('should generate the correctly sized file', function (done) {
+    var path = uuid.v4();
+    const size = sizeParser("20mb");
+    fileinator.writeFile(size, path)
+      .on('done', () => {
+        console.log('huh');
+        fs.stat(path, (err, stats) => {
+          if (err) throw err;
+          assert.equal(stats.size, size);
+          fs.unlink(path, () => {
+            done();
+          });
+        });
+      });
+  });
+
+  it('should handle noneven chunk sizes', function (done) {
+    const size = sizeParser("25mb");
+    var path = uuid.v4();
+    fileinator.writeFile(size, path)
+      .on('done', () => {
+        fs.stat(path, (err, stats) => {
+          if (err) throw err;
+          assert.equal(stats.size, size);
+          fs.unlink(path, () => {
+            done();
+          });
+        });
+      });
+  });
+
+  it('should report progress that adds up to the total', function (done) {
+    const size = sizeParser("25mb");
+    var path = uuid.v4();
+    var bytesWritten = 0;
+    fileinator.writeFile(size, path)
+      .on('progress', (data) => {
+        bytesWritten += data.bytesWritten;
+      }).on('done', () => {
+        assert.equal(size, bytesWritten);
+        fs.unlink(path, () => {
+          done();
+        });
+      });
+  });
+  
+});
